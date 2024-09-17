509507fc3120fa548882bb9751036f9f19018554
João Pedro <crepperdasrave@outlook.com>
2024-09-17 14:24:34 -0300

    Parei na Aula 68

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    {
+        System.out.println("Dentro do bloco de inicialização!");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for (int episodio : this.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios() {
+        return this.episodios;
+    }
+
+    public void setEpisodios(int[] episodios) {
+        this.episodios = episodios;
+    }
+}
