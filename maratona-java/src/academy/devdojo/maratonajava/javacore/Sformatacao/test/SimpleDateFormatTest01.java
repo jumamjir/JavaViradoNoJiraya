24ba67f8ea617179fcb88babed16526b50700097
João Pedro Madeira <crepperdasrave@outlook.com>
2024-11-01 17:21:21 -0300

    Parei na Aula 119

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+        String pattern = "'Amsterdam' dd 'de' MMMM 'de' yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        System.out.println(sdf.format(new Date()));
+        try {
+            System.out.println(sdf.parse("Amsterdam 01 de novembro de 2024"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
