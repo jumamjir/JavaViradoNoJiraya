50762dd9135a43659a6776f6b87090b3dc2144f5
João Pedro Madeira <crepperdasrave@outlook.com>
2024-11-07 10:57:15 -0300

    Aula 140

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//File
+//FileWriter
+//FileReader
+//BufferedWriter
+//BufferedReader
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write("O DevDojo é daora, é o melhor curso do brasillllll\nContinuando a cantoria na próxima linha\n");
+            fw.flush();
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
