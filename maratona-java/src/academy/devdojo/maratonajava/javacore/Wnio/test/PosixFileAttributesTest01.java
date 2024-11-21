e3fd640215169ed5425085aca493137eb390a405
Joao Pedro <crepperdasrave@outlook.com>
2024-11-20 22:12:03 -0300

    152 Parte 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/PosixFileAttributesTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributes;
+
+public class PosixFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("home/joaopedro/file.txt");
+        PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+        System.out.println(posixFileAttributes.permissions());
+    }
+}
