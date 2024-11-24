c25d1fdbf4aa0422c1505b88557fb97c4cf2e247
Joao Pedro <crepperdasrave@outlook.com>
2024-11-23 21:45:47 -0300

    Aula 168 03

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneListTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneListTest01.java
@@ -13,10 +13,15 @@ public class SmartphoneListTest01 {
         List<Smartphone> smartphones = new ArrayList<>(6);
         smartphones.add(s1);
         smartphones.add(s2);
-        smartphones.add(s3);
-        smartphones.clear();
+        smartphones.add(0,s3);
         for(Smartphone smartphone : smartphones) {
             System.out.println(smartphone);
         }
+
+        Smartphone s4 = new Smartphone("22222", "Pixel");
+
+        System.out.println(smartphones.contains(s4));
+        int indexSmartphone4 = smartphones.indexOf(s4);
+        System.out.println(indexSmartphone4);
     }
 }
