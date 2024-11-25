10579a6c0610a5926d370a22b844a2434a441fa4
João Pedro Madeira <crepperdasrave@outlook.com>
2024-11-25 15:26:54 -0300

    Aula 172

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
@@ -4,8 +4,15 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+class MangaByIdComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 public class MangaSortTest01 {
     public static void main(String[] args) {
         List<Manga> mangas = new ArrayList<>(6);
@@ -18,6 +25,12 @@ public class MangaSortTest01 {
             System.out.println(manga);
         }
         Collections.sort(mangas);
+        System.out.println("-------------------");
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+//        Collections.sort(mangas, new MangaByIdComparator());
+        mangas.sort(new MangaByIdComparator());
         System.out.println("-------------------");
         for (Manga manga : mangas) {
             System.out.println(manga);
