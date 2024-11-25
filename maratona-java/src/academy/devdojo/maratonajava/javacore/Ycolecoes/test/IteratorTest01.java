9af655ecdc1a1e878009f5101390da2e7b93df54
João Pedro Madeira <crepperdasrave@outlook.com>
2024-11-25 16:05:39 -0300

    Aula 175

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTest01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
+        mangas.add(new Manga(1L, "Bersek", 9.5, 5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
+        mangas.add(new Manga(3L, "Attack on Titan", 11.20, 2));
+        mangas.add(new Manga(2L, "Dragon Ball Z", 2.99, 0));
+
+//        Iterator<Manga> mangaIterator = mangas.iterator();
+//        while (mangaIterator.hasNext()) {
+//            if (mangaIterator.next().getQuantiade() == 0) {
+//                mangaIterator.remove();
+//            }
+//        }
+
+
+        mangas.removeIf(manga -> manga.getQuantiade() == 0);
+
+        System.out.println(mangas);
+    }
+}
