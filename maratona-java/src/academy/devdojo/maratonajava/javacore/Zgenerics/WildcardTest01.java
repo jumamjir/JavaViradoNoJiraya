002a2a19eda4d0fbd585322f7f570d67ffd341ea
Joao Pedro <crepperdasrave@outlook.com>
2024-11-28 08:50:20 -0300

    Aula 185

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/WildcardTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics;
+
+abstract class Animal {
+    public abstract void consulta();
+}
+
+class Cachorro extends Animal {
+    @Override
+    public void consulta() {
+        System.out.println("Consultando cachorro");
+    }
+}
+
+class Gato extends Animal {
+    @Override
+    public void consulta() {
+        System.out.println("Consultando Gato");
+    }
+}
+
+public class WildcardTest01 {
+    public static void main(String[] args) {
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        Gato[] gatos = {new Gato(), new Gato()};
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        Animal[] animals = {new Gato(), new Cachorro()};
+        Animal animal = new Gato();
+        printConsulta(animals);
+    }
+
+    private static void printConsulta(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+//        animals[1] = new Gato();
+    }
+}
