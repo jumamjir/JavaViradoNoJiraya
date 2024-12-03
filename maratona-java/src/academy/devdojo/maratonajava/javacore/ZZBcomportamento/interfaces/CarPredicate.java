8228508bb1c283d11a2f81627f8534ba566bef24
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-03 16:04:50 -0300

    Aula 196

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,7 +2,10 @@ package academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // anônimas , funções, conciso
     boolean test(Car car);
-
+    // (parametro) -> <expressão>
+    //( Car car) -> car.getColor().equals("green");
 }
