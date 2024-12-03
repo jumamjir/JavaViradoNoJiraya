0dcd6c305eae8bcb8bdb907f1b07e20d91e6148d
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-03 14:13:45 -0300

    Aula 192

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAclassesInternas/test/OuterClassesTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAclassesInternas/test/OuterClassesTest02.java
@@ -17,6 +17,6 @@ public class OuterClassesTest02 {
 
     public static void main(String[] args) {
         OuterClassesTest02 outer = new OuterClassesTest02();
-        outer.print();
+        outer.print(" ");
     }
 }
