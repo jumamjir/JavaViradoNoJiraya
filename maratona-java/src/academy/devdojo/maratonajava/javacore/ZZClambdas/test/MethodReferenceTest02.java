234f55b95b1dbc3a475052e89a6907f16d194528
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-04 15:58:30 -0300

    Aula 200

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -5,13 +5,14 @@ import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
 
 import java.util.ArrayList;
 import java.util.List;
-// Reference to a static method
-public class MethodReferenceTest01 {
+
+//Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
     public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
         List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One Piece", 900), new Anime("Naruto", 500)));
-//        Collections.sort(animeList, (a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
-        animeList.sort(AnimeComparators::compareByTitle);
-        animeList.sort(AnimeComparators::compareByEpisodes);
+        animeList.sort(animeComparators :: compareByEpisodesNonStatic);
         System.out.println(animeList);
     }
+
 }
