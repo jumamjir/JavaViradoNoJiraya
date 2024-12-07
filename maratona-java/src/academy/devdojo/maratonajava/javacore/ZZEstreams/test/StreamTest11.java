5633929dae2b067d32d0f14b1f20231c1b598cba
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-07 11:08:23 -0300

    Aula 213 part 03

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest11.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest11.java
@@ -4,6 +4,7 @@ import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,12 +24,17 @@ public class StreamTest11 {
         System.out.println(lightNovels.stream().count());
         System.out.println(lightNovels.stream().collect(Collectors.counting()));
 
-
         lightNovels.stream().max(Comparator.comparing(LightNovel:: getPrice)).ifPresent(System.out::println);
         lightNovels.stream().collect(Collectors.maxBy(Comparator.comparing(LightNovel::getPrice))).ifPresent(System.out::println);
 
-        lightNovels.stream().mapToDouble(LightNovel :: getPrice).sum();
+        System.out.println(lightNovels.stream().mapToDouble(LightNovel::getPrice).sum());
+        System.out.println(lightNovels.stream().collect(Collectors.summingDouble(LightNovel :: getPrice)));
 
+        lightNovels.stream().mapToDouble(LightNovel::getPrice).average().ifPresent(System.out::println);
+        System.out.println(lightNovels.stream().collect(Collectors.averagingDouble(LightNovel :: getPrice)));
+
+        DoubleSummaryStatistics collect = lightNovels.stream().collect(Collectors.summarizingDouble(LightNovel::getPrice));
+        System.out.println(collect);
 
     }
 }
