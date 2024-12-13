1da0b25e26efb749f898a6ec488d285349938d98
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-13 14:47:36 -0300

    Aula 226

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -2,8 +2,8 @@ package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
 
 import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Account;
 
-public class ThreadAccountTest01 implements Runnable{
-    private Account account = new Account();
+public class ThreadAccountTest01 implements Runnable {
+    private final Account account = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
@@ -12,24 +12,30 @@ public class ThreadAccountTest01 implements Runnable{
         t1.start();
         t2.start();
     }
+
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
             withdrawal(10);
-            if(account.getBalance() < 0) {
+            if (account.getBalance() < 0) {
                 System.out.println("FALIDO");
             }
         }
     }
 
     private void withdrawal(int amount) {
-        if (account.getBalance() >= amount) {
-            System.out.println(getThreadName() + "Está indo sacar dinheiro!");
-            account.withdrawal(amount);
-            System.out.println(getThreadName() + "completou o saque,valor atual da conta: " + account.getBalance());
-        } else {
-            System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque" + account.getBalance());
+        System.out.println(getThreadName() + " ##### fora do synchronized");
+        synchronized (account) {
+            System.out.println(getThreadName() + " ****** dentro do synchronized");
+            if (account.getBalance() >= amount) {
+                System.out.println(getThreadName() + "Está indo sacar dinheiro!");
+                account.withdrawal(amount);
 
+                System.out.println(getThreadName() + "completou o saque,valor atual da conta: " + account.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+
+            }
         }
     }
 
