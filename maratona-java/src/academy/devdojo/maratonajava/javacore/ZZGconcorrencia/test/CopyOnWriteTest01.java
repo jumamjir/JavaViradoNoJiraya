991d1f546f3ea97421ab47fe4dfba0fdddf793d6
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-18 21:17:20 -0300

    Aula 233 comment 1

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CopyOnWriteTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+final class Anime {
+    private final String name;
+
+    Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class CopyOnWriteTest01 {
+    public static void main(String[] args) {
+
+    }
+}
