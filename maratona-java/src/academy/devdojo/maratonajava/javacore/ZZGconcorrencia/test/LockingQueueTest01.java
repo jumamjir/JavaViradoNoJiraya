c9a81a0842d614cd829bac18ece694665606e28b
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-19 17:28:26 -0300

    Aula 233 comment 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/LockingQueueTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class LockingQueueTest01 {
+    public static void main(String[] args) {
+        BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+
+    }
+}
