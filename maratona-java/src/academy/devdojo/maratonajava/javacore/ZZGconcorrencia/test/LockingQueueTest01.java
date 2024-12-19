d94498f01822f8350baa186815ed5560a6c9d5c6
João Pedro Madeira <crepperdasrave@outlook.com>
2024-12-19 17:32:47 -0300

    Aula 233 comment 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/LockingQueueTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/LockingQueueTest01.java
@@ -4,8 +4,12 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class LockingQueueTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+        bq.put("William");
+        System.out.printf("%s added the value %s%n", Thread.currentThread().getName(), bq.peek());
+        System.out.println("Trying to add another value");
+
 
     }
 }
