405b1ae07eecdfefe8131a2ed6131c58599781c2
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-20 20:40:47 -0300

    Aula 236 comment 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/LinkedTransferQueueTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TransferQueue;
+
+public class LinkedTransferQueueTest01 {
+    public static void main(String[] args) {
+//        ConcurrentLinkedQueue, SynchronousQueue, LinkedBlockingQueue
+        TransferQueue<Object> objects = new LinkedTransferQueue<>();
+        System.out.println();
+    }
+}
