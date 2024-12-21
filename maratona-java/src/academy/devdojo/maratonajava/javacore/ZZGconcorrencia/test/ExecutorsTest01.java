af104414ec77a2aaa0d970be3963959d40436cff
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-21 18:52:09 -0300

    Aula 236 Correct one comment 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ExecutorsTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.TimeUnit;
+
+class Printer implements Runnable {
+    private final int num;
+
+    public Printer(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s inicio: %d%n", Thread.currentThread().getName(), num);
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("%s finalizou%n", Thread.currentThread().getName());
+    }
+}
+
+public class ExecutorsTest01 {
+    public static void main(String[] args) {
+
+    }
+}
