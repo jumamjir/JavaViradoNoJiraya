7f4612e2b6ec105cc433328bf2ac42bf8734111b
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-23 20:02:40 -0300

    Aula 238

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadPoolTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadPoolTest01.java
@@ -14,15 +14,21 @@ public class ScheduledThreadPoolTest01 {
     private static void beeper() {
         Runnable r = () -> {
             System.out.println(LocalTime.now().format(formatter) + " beep");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         };
 
 //        executor.schedule(r, 5, TimeUnit.SECONDS);
         ScheduledFuture<?> scheduleWithFixedDelay = executor.scheduleWithFixedDelay(r, 1, 5, TimeUnit.SECONDS);
+//        ScheduledFuture<?> scheduleWithFixedDelay = executor.scheduleAtFixedRate(r, 1, 5, TimeUnit.SECONDS);
         executor.schedule(() -> {
             System.out.println("Cancelando o scheduleWithFixedDelay");
             scheduleWithFixedDelay.cancel(false);
             executor.shutdown();
-        }, 10, TimeUnit.SECONDS);
+        }, 20, TimeUnit.SECONDS);
     }
 
     public static void main(String[] args) {
