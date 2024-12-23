5638c4bd447e1f7b01d2d744c7b1cd6487725ce1
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-23 19:51:18 -0300

    Aula 237 comment 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadPoolTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadPoolTest01.java
@@ -1,17 +1,26 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ScheduledThreadPoolTest01 {
     private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     private static void beeper() {
         Runnable r = () -> {
-            System.out.println(LocalTime.now() + " beep");
+            System.out.println(LocalTime.now().format(formatter) + " beep");
         };
+
+        executor.schedule(r, 5, TimeUnit.SECONDS);
+
     }
 
+    public static void main(String[] args) {
+        System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        beeper();
+    }
 }
