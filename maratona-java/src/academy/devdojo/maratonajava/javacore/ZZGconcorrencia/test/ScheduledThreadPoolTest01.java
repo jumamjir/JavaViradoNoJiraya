4d50c4c051d059013f97ce993409124e283e465e
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-23 19:57:11 -0300

    Aula 237 comment 03

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadPoolTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadPoolTest01.java
@@ -4,6 +4,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class ScheduledThreadPoolTest01 {
@@ -15,12 +16,17 @@ public class ScheduledThreadPoolTest01 {
             System.out.println(LocalTime.now().format(formatter) + " beep");
         };
 
-        executor.schedule(r, 5, TimeUnit.SECONDS);
-
+//        executor.schedule(r, 5, TimeUnit.SECONDS);
+        ScheduledFuture<?> scheduleWithFixedDelay = executor.scheduleWithFixedDelay(r, 1, 5, TimeUnit.SECONDS);
+        executor.schedule(() -> {
+            System.out.println("Cancelando o scheduleWithFixedDelay");
+            scheduleWithFixedDelay.cancel(false);
+            executor.shutdown();
+        }, 10, TimeUnit.SECONDS);
     }
 
     public static void main(String[] args) {
-        System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        System.out.println(LocalTime.now().format(formatter));
         beeper();
     }
 }
