b6704c5dcd4967a0f2748e030433cf6bfedd210c
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-25 09:58:51 -0300

    Aula 242 comment 03

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CompletableFutureTest03.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/CompletableFutureTest03.java
@@ -3,6 +3,11 @@ package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service.StoreServiceDeprecated;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CompletableFutureTest03 {
     public static void main(String[] args) {
@@ -12,8 +17,17 @@ public class CompletableFutureTest03 {
 
     private static void searchPricesASyncCompletableFuture(StoreServiceDeprecated storeService) {
         long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeService.getPriceSync(s), executor))
+                .collect(Collectors.toList());
 
+        List<Double> prices = completableFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
+        System.out.println(prices);
         long end = System.currentTimeMillis();
         System.out.printf("Time passed to searchPricesSync: %dms%n", (end - start));
     }
