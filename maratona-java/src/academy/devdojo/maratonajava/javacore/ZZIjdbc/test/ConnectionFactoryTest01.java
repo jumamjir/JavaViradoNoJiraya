81dc017878d78be0b56d576aa324319607b67c02
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2024-12-29 13:23:18 -0300

    Aula 259

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -2,10 +2,17 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
+//        log.info("info");
+//        log.debug("debug");
+//        log.warn("warn");
+//        log.error("error");
+//        log.trace("trace");
     }
 }
